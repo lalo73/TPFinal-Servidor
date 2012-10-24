926aab643b2df1228532384951efb97dded8adf6
lalo73 <leandro.gomez73@yahoo.com.ar>
2012-10-24 16:15:30 -0300

    Segundo copmmit

---
+++ src/server/IEvent.java
@@ -0,0 +1,9 @@
+package server;
+
+import java.util.Date;
+
+public interface IEvent {
+
+	public void actuar(Date d, IAcount c);
+	
+}
--- src/server/IServer.java
+++ src/server/IServer.java
@@ -1,5 +1,7 @@
 package server;
 
 public interface IServer {
+	
+	public String returnEmail();
 
 }
